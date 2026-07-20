e8612ed0a672f3043754af12b5ad4a8f725f39ee
Jalla Lavanya <lavanya123jalla@gmail.com>
2026-07-20 15:33:15 +0530

    Update test directory path in Playwright configuration to point to API layer tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ dotenv.config();
  */
 export default defineConfig({
   timeout: 60000,
-  testDir: './tests/ui-layer/e2e/',
+  testDir: './tests/api-layer/e2e/',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
